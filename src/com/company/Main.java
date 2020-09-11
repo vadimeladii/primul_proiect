1d31e7745d63e0bbea9710865dd284776f80ded2
vadim eladii <vadimeladii@gmail.com>
2020-09-11 16:38:33 +0300

    Am adaugat unele variabile primitive si o conditie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 10;
+        int b = 20;
+        char c = 'a';
+        Boolean d = true;
+
+        if (d) {
+            System.out.println("test");
+        }
     }
 }
